adf2f9f13ce631c653b4956d29e0ce36940280f3
Richard Younes <richardyounes01@gmail.com>
2024-06-12 23:25:58 +0300

    Upload images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ccvkdunhouafzzsctmib.supabase.co';
+export const supabaseUrl = 'https://ccvkdunhouafzzsctmib.supabase.co';
 
 const supabaseKey =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdmtkdW5ob3VhZnp6c2N0bWliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwNDIzMzEsImV4cCI6MjAzMzYxODMzMX0.ak1dRaQHVN3MiquqdnWtn-vdresfD1dnn7Z9vXX7SNA';
