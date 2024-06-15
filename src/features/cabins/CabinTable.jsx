21b8ad6c46e8e5caaefe7ee211351ec458fc5f90
Richard Younes <richardyounes01@gmail.com>
2024-06-15 15:14:12 +0300

    Build a reusable context Menu

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Spinner from '../../ui/Spinner';
 import CabinRow from './CabinRow';
 import { useCabins } from './useCabins';
 import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
 
 // const Table = styled.div`
 // 	border: 1px solid var(--color-grey-200);
@@ -13,43 +14,44 @@ import Table from '../../ui/Table';
 // 	overflow: hidden;
 // `;
 
-const TableHeader = styled.header`
-	display: grid;
-	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-	column-gap: 2.4rem;
-	align-items: center;
+// const TableHeader = styled.header`
+// 	display: grid;
+// 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+// 	column-gap: 2.4rem;
+// 	align-items: center;
 
-	background-color: var(--color-grey-50);
-	border-bottom: 1px solid var(--color-grey-100);
-	text-transform: uppercase;
-	letter-spacing: 0.4px;
-	font-weight: 600;
-	color: var(--color-grey-600);
-	padding: 1.6rem 2.4rem;
-`;
+// 	background-color: var(--color-grey-50);
+// 	border-bottom: 1px solid var(--color-grey-100);
+// 	text-transform: uppercase;
+// 	letter-spacing: 0.4px;
+// 	font-weight: 600;
+// 	color: var(--color-grey-600);
+// 	padding: 1.6rem 2.4rem;
+// `;
 
 function CabinTable() {
 	const { isLoading, cabins } = useCabins();
 
 	if (isLoading) return <Spinner />;
-	let x = [];
 
 	return (
-		<Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-			<Table.Header>
-				<div></div>
-				<div>Cabin</div>
-				<div>Capacity</div>
-				<div>Price</div>
-				<div>Discount</div>
-				<div></div>
-			</Table.Header>
+		<Menus>
+			<Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+				<Table.Header>
+					<div></div>
+					<div>Cabin</div>
+					<div>Capacity</div>
+					<div>Price</div>
+					<div>Discount</div>
+					<div></div>
+				</Table.Header>
 
-			<Table.Body
-				data={cabins}
-				render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
-			/>
-		</Table>
+				<Table.Body
+					data={cabins}
+					render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+				/>
+			</Table>
+		</Menus>
 	);
 }
 
