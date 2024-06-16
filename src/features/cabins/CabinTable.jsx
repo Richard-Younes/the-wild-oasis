c41038906197456dd7c60dc88cf5f10b915728a9
Richard Younes <richardyounes01@gmail.com>
2024-06-16 21:58:41 +0300

    Sort Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -37,6 +37,7 @@ function CabinTable() {
 
 	if (isLoading) return <Spinner />;
 
+	// 1) FILTER
 	const filterValue = searchParams.get('discount') || 'all';
 
 	let filteredCabins;
@@ -48,6 +49,17 @@ function CabinTable() {
 
 	if (filterValue === 'with-discount')
 		filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+	// 2) SORT
+
+	const sortBy = searchParams.get('sortBy') || 'name-asc';
+	const [field, direction] = sortBy.split('-');
+
+	const modifier = direction === 'asc' ? 1 : -1;
+
+	const sortedCabins = filteredCabins.sort(
+		(a, b) => (a[field] - b[field]) * modifier
+	);
 
 	return (
 		<Menus>
@@ -62,7 +74,7 @@ function CabinTable() {
 				</Table.Header>
 
 				<Table.Body
-					data={filteredCabins}
+					data={sortedCabins}
 					render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
 				/>
 			</Table>
